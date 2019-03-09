3bae7cf7a3bb0eddb2350b5fcd60ab10db741897
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-03-09 19:11:24 +0100

    project names downloaded from github

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,23 +1,44 @@
 <template>
     <div class="home">
         <h1 class="greeting">Projects</h1>
+        <ul class="main-points">
+            <li v-for="project in projects" :key="project.name">
+                <a class="info" v-bind:href="project.link">{{project.name}}</a>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+const myGithubLink = "https://github.com/przemekBielak/";
+
 export default {
     name: 'projects',
     data: function() {
         return {
-            projectName: [],
-            projectDescription: [],
+            projects: [
+                {
+                    name: '',
+                    link: '',
+                    description: ''
+                }
+            ],
         }
     },
     methods: {
         getProjects: function() {
-            for(var i = 0; i < 10; i++) {
-                this.projectName[i] = i;
-            }
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() { 
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                    var contentJSON = JSON.parse(xmlHttp.responseText);
+                    this.projects = contentJSON.map(item => ({
+                        name: item.name, 
+                        link: myGithubLink + item.name
+                    }));
+                }
+            }.bind(this)
+            xmlHttp.open("GET", "https://api.github.com/users/przemekBielak/repos", true);
+            xmlHttp.send(null);
         }
     },
     created: function() {
