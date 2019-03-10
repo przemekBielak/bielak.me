e8f3cd8bb89d7e81461866231cebdf812c3f1449
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-03-10 15:16:09 +0100

    fixed language list

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="home" v-if="projects[0].name">
+    <div class="home" v-if="loaded">
         <h1 class="greeting">Projects.</h1>
         <ul class="main-points">
             <li v-for="project in projects" :key="project.name">
                 <a class="info" v-bind:href="project.link">{{project.name}}</a>
-                /{{project.language}}
+                <span v-for="(val, key) in project.languages" :key="key"> {{key}} </span>
             </li>
         </ul>
+        <p class="remarks">
+            *Information is automatically downloaded using GitHub API.
+        </p>
     </div>
 </template>
 
@@ -24,9 +27,11 @@ export default {
                 {
                     name: null,
                     link: null,
-                    language: null
+                    languageLink: null,
+                    languages: null
                 }
             ],
+            loaded: false,
         }
     },
     methods: {
@@ -38,12 +43,14 @@ export default {
                     this.projects = contentJSON.map(item => ({
                         name: item.name, 
                         link: myGithubLink + item.name,
-                        language: item.languages_url
+                        languageLink: item.languages_url,
+                        languages: {}
                     }));
                     for(var i = 0; i < this.projects.length; i++) {
-                        this.getProjectsLangs(this.projects[i].language, i);
+                        this.getProjectsLangs(this.projects[i].languageLink, i);
                     }
                 }
+                this.loaded = true;
             }.bind(this)
             xmlHttp.open("GET", "https://api.github.com/users/przemekBielak/repos", true);
             xmlHttp.send(null);
@@ -53,7 +60,7 @@ export default {
             xmlHttp.onreadystatechange = function() { 
                 if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                     var contentJSON = JSON.parse(xmlHttp.responseText);
-                    this.projects[i].language = contentJSON;
+                    this.projects[i].languages = contentJSON;
                 }
             }.bind(this)
             xmlHttp.open("GET", link, true);
@@ -65,3 +72,11 @@ export default {
     }
 }
 </script>
+
+
+<style>
+.remarks {
+    margin-top: 40px;
+    font-size: 1.6em;
+}
+</style>
