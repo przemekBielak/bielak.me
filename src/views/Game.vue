d51c29d7223845638746cbb89b8ce137c78131dd
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-09-26 15:58:35 +0200

    moved everything to vue component

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,129 +7,123 @@
 <script>
 import router from "../router";
 
-let canvas;
-let ctx;
-
-let x;
-let y;
-const ballRadius = 10;
-let dx = 2;
-let dy = -2;
-
-const paddleHeight = 10;
-const paddleWidth = 75;
-let paddleX;
-let paddledx = 7;
-
-let leftPressed = false;
-let rightPressed = false;
-
-let points = 0;
-
-function keyDownHandler(event) {
-  if (event.keyCode == 39) {
-    rightPressed = true;
-  } else if (event.keyCode == 37) {
-    leftPressed = true;
-  }
-}
-
-function keyUpHandler(event) {
-  if (event.keyCode == 39) {
-    rightPressed = false;
-  } else if (event.keyCode == 37) {
-    leftPressed = false;
-  }
-}
-
-function drawPaddle() {
-  ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = "blue";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "red";
-  ctx.fill();
-  ctx.closePath();
-}
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
 export default {
   name: "game",
   data: function() {
     return {
-        gameover: false
+        gameover: false,
+        canvas: 0,
+        ctx: 0,
+        x: 0,
+        y: 0,
+        ballRadius: 10,
+        dx: 2,
+        dy: -2,
+        paddleHeight: 10,
+        paddleWidth: 75,
+        paddleX : 0,
+        paddledx: 7,
+        points: 0,
+        leftPressed: false,
+        rightPressed: false,
     };
   },
   mounted: function() {
-    canvas = document.getElementById("myCanvas");
-    ctx = canvas.getContext("2d");
+    // key handlers
+    document.addEventListener("keydown", this.keyDownHandler, false);
+    document.addEventListener("keyup", this.keyUpHandler, false);
 
-    x = canvas.width / 2;
-    y = canvas.height / 2;
+    // initialization
+    this.canvas = document.getElementById("myCanvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.x = this.canvas.width / 2;
+    this.y = this.canvas.height / 2;
 
-    paddleX = (canvas.width - paddleWidth) / 2;
+    this.paddleX = (this.canvas.width - this.paddleWidth) / 2;
 
     this.draw();
   },
-  updated: function() {
-    if (this.gameover) {
-      router.push("home");
-    }
-  },
-  methods: {
-    draw: function() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawBall();
-      drawPaddle();
 
-      if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx = -dx;
+  methods: {
+    keyDownHandler: function(event) {
+        if (event.keyCode == 39) {
+            this.rightPressed = true;
+        } else if (event.keyCode == 37) {
+            this.leftPressed = true;
+        }
+    },
+
+    keyUpHandler: function(event) {
+        if (event.keyCode == 39) {
+            this.rightPressed = false;
+        } else if (event.keyCode == 37) {
+            this.leftPressed = false;
+        }
+    },
+
+    drawPaddle: function() {
+        this.ctx.beginPath();
+        this.ctx.rect(this.paddleX, this.canvas.height - this.paddleHeight, this.paddleWidth, this.paddleHeight);
+        this.ctx.fillStyle = "blue";
+        this.ctx.fill();
+        this.ctx.closePath();
+    },
+
+    drawBall: function() {
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2);
+        this.ctx.fillStyle = "red";
+        this.ctx.fill();
+        this.ctx.closePath();
+    },
+
+    draw: function() {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.drawBall();
+      this.drawPaddle();
+
+      if (this.x + this.dx > this.canvas.width - this.ballRadius || this.x + this.dx < this.ballRadius) {
+        this.dx = -this.dx;
       }
 
-      if (y + dy < ballRadius) {
-        dy = -dy;
-      } else if (y + dy > canvas.height - ballRadius) {
-        ctx.font = "30px Arial";
-        ctx.fillText("Score: " + points, canvas.width / 2, canvas.height / 2);
+      if (this.y + this.dy < this.ballRadius) {
+        this.dy = -this.dy;
+      } else if (this.y + this.dy > this.canvas.height - this.ballRadius) {
+        this.ctx.font = "30px Arial";
+        this.ctx.fillText("Score: " + this.points, this.canvas.width / 2, this.canvas.height / 2);
 
         this.gameover = true;
-        x = canvas.width / 2;
-        y = canvas.height / 2;
-        points = 0;
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height / 2;
+        this.points = 0;
       }
 
-      if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += paddledx;
-      } else if (leftPressed && paddleX > 0) {
-        paddleX -= paddledx;
+      if (this.rightPressed && this.paddleX < this.canvas.width - this.paddleWidth) {
+        this.paddleX += this.paddledx;
+      } else if (this.leftPressed && this.paddleX > 0) {
+        this.paddleX -= this.paddledx;
       }
 
       if (
-        x > paddleX &&
-        x < paddleX + paddleWidth &&
-        y == canvas.height - ballRadius - paddleHeight
+        this.x > this.paddleX &&
+        this.x < this.paddleX + this.paddleWidth &&
+        this.y == this.canvas.height - this.ballRadius - this.paddleHeight
       ) {
-        points++;
-        dy = -dy;
-        console.log(points);
+        this.points++;
+        this.dy = -this.dy;
+        console.log(this.points);
       }
 
-      x += dx;
-      y += dy;
+      this.x += this.dx;
+      this.y += this.dy;
 
       if (!this.gameover) {
         requestAnimationFrame(this.draw);
       }
       else {
-        router.push({ path: '/' })
+          setTimeout(() => {
+              router.push({ path: '/' })
+          }, 3000);
       }
     }
   }
